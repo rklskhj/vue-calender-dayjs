0ad57f0b2eacf3acd0c00303ba46736947525033
rklskhj <rklskhj2@naver.com>
2023-01-26 03:32:39 +0900

    1st vue calendar dayjs prac

---
+++ src/components/CalendarDateIndicator.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="calendar-date-indicator">{{ selectedMonth }}</div>
+</template>
+<script>
+export default {
+  props: {
+    selectedDate: {
+      type: Object,
+      required: true,
+    },
+  },
+  computed: {
+    selectedMonth() {
+      return this.selectedDate.format("YYYY MMMM");
+    },
+  },
+};
+</script>
+<style scoped>
+.calendar-date-indicator {
+  font-size: 24px;
+  font-weight: 600;
+  color: var(--grey-00);
+}
+</style>
---
+++ src/components/CalendarWeekdays.vue
@@ -0,0 +1,29 @@
+<template>
+  <ol class="day-of-week">
+    <li v-for="weekday in weekdays" :key="weekday">{{ weekday }}</li>
+  </ol>
+</template>
+<script>
+const WEEKDAYS = ["월", "화", "수", "목", "금", "토", "일"];
+
+export default {
+  name: "CalendarWeekdays",
+  computed: {
+    weekdays() {
+      return WEEKDAYS;
+    },
+  },
+};
+</script>
+<style scoped>
+.day-of-week {
+  display: grid;
+  grid-template-columns: repeat(7, 1fr);
+}
+
+.day-of-week > * {
+  text-align: right;
+  padding-right: 5px;
+  font-weight: bold;
+}
+</style>
